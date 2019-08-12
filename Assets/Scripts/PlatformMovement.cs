6f4947dc863a8ed9273e4638af83feeccbcfacfa
Grant Furgiuele <grant.furgiuele@gmail.com>
2019-08-12 16:45:28 -0400

    Added a couple of player sounds, footsteps and jumping sound Added a couple of prefabs that might be helpful, indicating ground and a moving platform Added enemy prefab with some automatic enemy movement Added singleton pattern to GameManager

---
+++ Assets/Scripts/PlatformMovement.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMovement : MonoBehaviour
+{
+    public float movementSpeed;
+
+    public float distanceFromCenter;
+
+    public enum MovementAxis
+    {
+        Y_AXIS, X_AXIS
+    }
+
+    public MovementAxis movementAxis;
+    float direction = 1;
+
+    Vector2 startingPosition;
+    Vector2 velocity;
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingPosition = transform.position;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        if(movementAxis == MovementAxis.Y_AXIS) {
+            if (direction > 0) {
+                velocity = Vector2.Lerp(velocity, Vector2.up, movementSpeed);
+            } else {
+                velocity = Vector2.Lerp(velocity, Vector2.down, movementSpeed);
+            }
+            if (transform.position.y > (startingPosition.y + distanceFromCenter)) {
+                    direction = -1;
+            } else if (transform.position.y < (startingPosition.y - distanceFromCenter)) {
+                    direction = 1;
+            }
+        }
+
+        if(movementAxis == MovementAxis.X_AXIS) {
+            if (direction > 0) {
+                velocity = Vector2.Lerp(velocity, Vector2.right, movementSpeed);
+            } else {
+                velocity = Vector2.Lerp(velocity, Vector2.left, movementSpeed);
+            }
+            if (transform.position.x > (startingPosition.x + distanceFromCenter)) {
+                    direction = -1;
+            } else if (transform.position.x < (startingPosition.x - distanceFromCenter)) {
+                    direction = 1;
+            }
+        }
+
+        float newPosX = rb.position.x + velocity.x * movementSpeed * Time.deltaTime;
+        float newPosY = rb.position.y + velocity.y * movementSpeed * Time.deltaTime;
+        Vector2 newPos = new Vector2(newPosX, newPosY);
+        rb.MovePosition(newPos);
+    }
+
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -45,6 +45,7 @@ public class PlayerMovement : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        psounds = GetComponent<PlayerSound>();
     }
 
     // Update is called once per frame
